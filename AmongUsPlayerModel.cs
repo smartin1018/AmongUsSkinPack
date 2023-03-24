9d099b9348f2794ce30e0d80d09c25500418703d
Martoph <sager1018@gmail.com>
2023-03-23 23:48:05 -0500

    Populate files

--- AmongUsPlayerModel.cs
+++ AmongUsPlayerModel.cs
@@ -1,11 +1,25 @@
-﻿using UnityEngine;
+﻿using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using KitchenPlayerModelLib;
+using UnityEngine;
 
-namespace PlateUpAmogus
+namespace KitchenAmongUs
 {
     public class AmongUsPlayerModel : CustomPlayerModel
     {
         public override string UniqueNameID => "AmongUsPlayerModel";
-        public override GameObject Prefab => (GameObject)Main.Bundle.LoadAsset("assets/mods/assetbundle/amogus_plateup_v2.prefab");
+        public override GameObject Prefab => (GameObject)Mod.Bundle.LoadAsset("assets/mod/amogus_plateup_v2.prefab");
+
         public override Mesh Mesh => Prefab.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
+        
+        private Material AmongUsVisor => CustomMaterials.GetCustomMaterial("AmongUsVisor");
+        
+        private Material AmongUsVisorSeal => CustomMaterials.GetCustomMaterial("AmongUsVisorSeal");
+
+        public override Material[] Materials => new[]
+        {
+            null, AmongUsVisorSeal, AmongUsVisor
+        };
     }
 }
